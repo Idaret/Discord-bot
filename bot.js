9a62a45065b36cf8b42573c1ed48696416893a4d
lwoloszyn <lukaszw2000@tlen.pl>
2018-04-08 11:22:48 +0200

    added readme and license, changed id of streamer to his name

--- bot.js
+++ bot.js
@@ -2,50 +2,61 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const request = require('request');
 const data = require('./auth.json');
-
-let channelID = data.discordChannelID;
 let currentGame = null;
-let authDiscord = data.tokenDiscordBot;
-let intervalTime = data.refreshTime;
-let options = {
-    url: 'https://api.twitch.tv/kraken/streams/' + data.twitchChannelID, 
-    headers: {
-      'Accept': 'application/vnd.twitchtv.v5+json',
-      'Client-ID': data.twitchClientID
-    }
-  };
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  let channel = client.channels.get(channelID);
-    setInterval(function(){
-        request(options,function(error, response, body){
-            if (!error && response.statusCode == 200) {
-                let info = JSON.parse(body);
-                if(info.stream === null)
-                {
-                    currentGame = null;
-                }
-                else{
-                    let newGame = info.stream.game, user = info.stream.channel.name;
-                    if(currentGame === null)
-                    {
-                        currentGame = newGame ;
-                        channel.send(`${user} is streaming ${currentGame} right now`);
+
+request({
+        url: 'https://api.twitch.tv/kraken/users?login=' + data.twitchChannelName,
+        headers: {
+            'Accept': 'application/vnd.twitchtv.v5+json',
+            'Client-ID': data.twitchClientID
+        }
+    },
+    function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            let results = JSON.parse(body);
+            if (results._total != 0) {
+
+                let options = {
+                    url: 'https://api.twitch.tv/kraken/streams/' + results.users[0]._id,
+                    headers: {
+                        'Accept': 'application/vnd.twitchtv.v5+json',
+                        'Client-ID': data.twitchClientID
                     }
-    
-                    if(newGame !== currentGame && currentGame !== null)
-                    {
-                        currentGame = newGame ;
-                        channel.send(`${user} is now playing ${currentGame}`);
-                    }
-                }
-            
-              }
-        });
+                };
 
-    },intervalTime);
-  
-});
+                client.on('ready', () => {
+                    console.log(`Logged in as ${client.user.tag}!`);
+                    let channel = client.channels.get(data.discordChannelID);
+                    setInterval(function() {
+                        request(options, function(error, response, body) {
+                            if (!error && response.statusCode == 200) {
+                                let info = JSON.parse(body);
+                                if (info.stream === null) {
+                                    currentGame = null;
+                                } else {
+                                    let newGame = info.stream.game,
+                                        user = info.stream.channel.name;
+                                    if (currentGame === null) {
+                                        currentGame = newGame;
+                                        channel.send(`${user} is streaming ${currentGame} right now`);
+                                    }
 
-client.login(authDiscord);
+                                    if (newGame !== currentGame && currentGame !== null) {
+                                        currentGame = newGame;
+                                        channel.send(`${user} is now playing ${currentGame}`);
+                                    }
+                                }
+
+                            }
+                        });
+
+                    }, data.refreshTime);
+
+                });
+
+                client.login(data.tokenDiscordBot);
+
+            }
+        }
+    });
---
+++ readme.md
@@ -0,0 +1,41 @@
+# Project Title
+
+It's a bot that posts in discord channel whenever streamer starts streamind AND changes game
+
+## Getting Started
+
+You need computer with node v8, discord channel, discord bot. Copy auth.example.json to auth.json and fill all blank strings. Feel free to use my twitchClientID.
+
+### Running bot
+
+```
+npm run start
+```
+
+## Running the tests
+
+Explain how to run the automated tests for this system
+
+### Break down into end to end tests
+
+Explain what these tests test and why
+
+```
+Give an example
+```
+
+### And coding style tests
+
+Explain what these tests test and why
+
+```
+Give an example
+```
+
+## Deployment
+
+Add additional notes about how to deploy this on a live system
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
